8e1dcc2c00830b1cd7ff5abce5194fb5160b9e31
Fran Burgos <f.burgos.valdes@gmail.com>
2019-03-29 20:54:13 +0100

    Bus detection

--- bus-client.py
+++ bus-client.py
@@ -3,44 +3,34 @@ from requests_toolbelt.utils import formdata
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
 import json
+import time
 
-## REQUEST BUS STOP DATA
-url = 'https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetArriveStop.php'
-query = {
-	"idClient":"WEB.SERV.marc.aguilar@gmail.com",
-	"passKey":"A6C8FC19-2A93-4CCB-AA8C-D044CF02D934",
-	"idStop":"5089",
-	"cultureInfo":"ES"
-}
-resp = requests.post(
-    url,
-    data=formdata.urlencode(query),
-    headers={
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-)
-# TODO Verify response code
-# TODO Loop every X seconds
-print resp.json();
-# TODO Publish on Kafka
-producer = KafkaProducer(
-    bootstrap_servers='kafka:9092',
-    # Encode all values as JSON
-    value_serializer=lambda value: json.dumps(value).encode('utf-8'),
-)
+
 while True:
-    producer.send('EMTStopGeoBusData', value={'foo': 'bar'})
+    time.sleep(5)
+    ## REQUEST BUS STOP DATA
+    url = 'https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetArriveStop.php'
+    query = {
+        "idClient":"WEB.SERV.marc.aguilar@gmail.com",
+        "passKey":"A6C8FC19-2A93-4CCB-AA8C-D044CF02D934",
+        "idStop":"5089",
+        "cultureInfo":"ES"
+    }
+    resp = requests.post(
+        url,
+        data=formdata.urlencode(query),
+        headers={
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+    )
 
-# producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-# for i in range(1000):
-#     producer.send('EMTStopGeoBusData', {'foo': 'bar'})
-#producer = KafkaProducer(bootstrap_servers='localhost:9092')
-#for _ in range(100):
-#    producer.send('EMTStopGeoBusData', b'some_message_bytes')
-
-consumer = KafkaConsumer('EMTStopGeoBusData')
-for msg in consumer:
-    print (msg)
+    data = resp.json()
+    arrives = data['arrives']
+    for arrive in arrives:
+        if(arrive['lineId'] == "63"):
+            print arrive
+            if(arrive['busDistance'] < 2):
+                print "################## El autobus ha llegado a la parada! BusId: " + arrive['busId']
     
-
+    
 
