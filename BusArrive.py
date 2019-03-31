fea6a1b6d705cf517d73e2f3b39c7c4285a1af72
Marc Aguilar <marc@Marcs-MacBook-Pro.local>
2019-03-31 22:11:52 +0200

    Adding some mocks + initial calculation

---
+++ BusArrive.py
@@ -0,0 +1,6 @@
+class BusArrive:
+    def __init__(self, busId, arrivalTime, nextBusId, estimatedNextBusTime):
+        self.busId = busId
+        self.arrivalTime = arrivalTime
+        self.nextBusId = nextBusId
+        self.estimatedNextBusTime = estimatedNextBusTime
