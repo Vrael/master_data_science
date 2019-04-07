b7609a12e7d4d8148e2998c6e1c92fe89874c24f
Fran Burgos <f.burgos.valdes@gmail.com>
2019-04-07 21:00:00 +0200

    Add dataframe generator for clustering analysis

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-sudo docker-compose -f docker-compose.yml exec kafka kafka-console-consumer --bootstrap-server localhost:9092 --topic EMTStopGeoBusData --from-beginning
+# Descripción
+Aplicación para detectar analizar los retrasos en las paradas de autobus de la EMT e identificar zonas con conflictos circulatorios
+
+# Requisitos
+Python 2.7.15
+
+# Instalación
+python pip install
+
+# Ejecución
+python .\dataframe.py
+
+# Funcionamiento
+1. Lectura del fichero EMT_tiempos_espera.txt
+2. Cálculo de la diferencia entre tiempo real y tiempo estimado
+3. Cálculo del porcentaje de desviación
+4. Filtrado de las paradas que tienen retrasos
+5. Agrupación de los datos por parda y retrasos diarios
+6. Selección del 40% de las paradas que acumulan mayores retrasos
+7. Cruze de datos con su geo-posición en el fichero EMT_coordenadas_paradas.txt
+8. Escritura del fichero dataframe.txt para el análisis de cluster
---
+++ dataframe.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+## Genera un dataframe con los tiempos medios extremos de espera por         ##
+## cada parada                                                               ##
+###############################################################################
+
+import pandas as pd
+import numpy as np
+
+def calculateMaxWaittingBusStops():
+	print("\nCalcula diff = estimateWait - realWait. Negativo significa retraso en la parada\n")
+	df = pd.read_csv('.\EMT_tiempos_espera.txt', delimiter='\t', comment='#', names=['stop', 'line', 'hour', 'realWait', 'estimateWait'])
+	df['delay'] = df['realWait'] - df['estimateWait']
+	df['%desviation'] = 100 * (df['estimateWait'] - df['realWait']) / df['estimateWait']
+	print(df)
+	
+	print("\n########################################")
+	print("\nSe filtran para dejar solo los retrasos.\n")
+	delay = df['delay'] > 0
+	print(df[delay])
+	
+	print("\n########################################")
+	print("\nSe agrupan las desviaciones por dia.\n")
+	df2 = df[delay].groupby(['stop'], sort=False)[['delay']].sum()
+	print(df2)
+	
+	print("\n########################################")
+	count = df2.count()					# total rows grouped by bus stop
+	count40 = int(round(40*count/100,0))# 40%
+	print("\nSe selecciona el 40% (" + str(count40) + " paradas) con los retrasos mayores\n")
+	df3 = df2.nlargest(count40, 'delay')
+	print(df3)
+	
+	print("\n########################################")
+	print("\nSe cruza el resultado con las coordenadas de las paradas\n")
+	df4 = pd.read_csv('.\EMT_coordenadas_paradas.txt', delimiter='\t', comment='#', names=['stop', 'x', 'y', 'description'])
+	merged = df3.merge(df4, on='stop', how='inner')
+	print(merged)
+	
+	del df4['description']
+	df4.to_csv('.\dataframe.txt', sep='\t', index=False)
+
+def main():
+	calculateMaxWaittingBusStops()
+
+if __name__== "__main__":
+  main()
